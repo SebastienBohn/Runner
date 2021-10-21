741f0e3424919944d009b2e372374ea161315c3d
SebastienBohn <sebohn1505@gmail.com>
2021-10-21 08:27:59 +0200

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,38 @@
+package com.company;
+
+// add any usefull package line
+
+import javafx.application.Application;
+import javafx.geometry.Rectangle2D;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+
+
+public class Main extends Application{
+
+    public void start(Stage primaryStage){
+
+        primaryStage.setTitle("BOHN_Sebastien_Runner");
+        Group root = new Group();
+        Scene scene = new Scene(root, 600, 400);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+
+        Image spriteSheet = new Image("E:\\Documents\\ENSEA\\2A\\MajeureInfo\\ProgObjetJava\\Runner\\heros.png");
+        ImageView sprite = new ImageView(spriteSheet);
+        sprite.setViewport(new Rectangle2D(20, 0, 65, 100));
+        sprite.setX(200);
+        sprite.setY(300);
+        root.getChildren().add(sprite);
+
+
+    }
+    public static void main(String[] args) {
+        launch(args);
+        // write your code here
+    }
+}
